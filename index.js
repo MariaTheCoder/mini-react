4d67fb66ebde1a047e771f78d840128590fbeed7
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-07 07:20:59 +0200

    contain data in an array. When a new grid element is created, store its text content inside of a data object and push the data object to the data array

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const saveBtn = document.getElementById("save_btn");
 const outputContainer = document.getElementById("output_container");
 const deleteAllBtn = document.getElementById("delete_all_btn");
 
+/* data */
+const data = [];
+
 /* Save the content of the text area and create a p element which contains this content once the Save button is clicked */
 saveBtn.addEventListener("click", () => {
   const input = textArea.value;
@@ -17,6 +20,12 @@ saveBtn.addEventListener("click", () => {
   newDeleteIcon.innerText = "delete";
 
   outputContainer.append(newOutputElement, newEditIcon, newDeleteIcon);
+
+  const newDataElement = { id: data.length, text: "" };
+  newDataElement.text = input;
+  data.push(newDataElement);
+  console.log("new data element: ", newDataElement);
+  console.log("data: ", data);
 });
 
 /* Delete all content of the output container when the Delete All button is clicked */
