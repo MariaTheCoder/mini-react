3b8053c10657737b038a241f67fb37543afb63ae
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-07 06:38:22 +0200

    add comments to current javascript functions that explains what the functions to and when they are executed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const saveBtn = document.getElementById("save_btn");
 const outputContainer = document.getElementById("output_container");
 const deleteAllBtn = document.getElementById("delete_all_btn");
 
+/* Save the content of the text area and create a p element which contains this content once the Save button is clicked */
 saveBtn.addEventListener("click", () => {
   const input = textArea.value;
 
@@ -11,6 +12,7 @@ saveBtn.addEventListener("click", () => {
   outputContainer.appendChild(newOutputElement);
 });
 
+/* Delete all content of the output container when the Delete All button is clicked */
 deleteAllBtn.addEventListener("click", () => {
   outputContainer.innerHTML = "";
 });
