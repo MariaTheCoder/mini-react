ed09954302eddc2ae0fb3bf84e9ddad0947b9080
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-07 06:23:58 +0200

    instead of logging the textarea content onto the console, create a p element with said content and append it to the output container upon button click

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const textArea = document.getElementById("text_area");
 const saveBtn = document.getElementById("save_btn");
+const outputContainer = document.getElementById("output_container");
 
 saveBtn.addEventListener("click", () => {
   const input = textArea.value;
 
-  console.log("new text input was loaded: ", input);
+  const newOutputElement = document.createElement("p");
+  newOutputElement.innerText = input;
+  outputContainer.appendChild(newOutputElement);
 });
