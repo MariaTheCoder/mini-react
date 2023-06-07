2745a1ad239df776ebf54cd6180e8e667c577011
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-07 06:17:50 +0200

    run a quick test in javascript to see if the new input of the textfield is saved correctly into a constant variable upon button click

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const textArea = document.getElementById("text_area");
+const saveBtn = document.getElementById("save_btn");
+
+saveBtn.addEventListener("click", () => {
+  const input = textArea.value;
+
+  console.log("new text input was loaded: ", input);
+});
